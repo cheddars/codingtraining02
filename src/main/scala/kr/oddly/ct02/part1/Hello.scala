aca68a4fee397d6c2d8a7f751910dc5a54f91ef4
cheddar <nezahrish@gmail.com>
2016-11-04 01:47:49 +0900

    challenge #01

--- src/main/scala/kr/oddly/ct02/part1/Hello.scala
+++ src/main/scala/kr/oddly/ct02/part1/Hello.scala
@@ -4,19 +4,6 @@ import scala.io.StdIn._
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val name = getName()
-    say(concat(name))
-  }
-  
-  def getName(): String = {
-    readLine("What is your name? ")
-  }
-  
-  def concat(name: String): String = {
-    "Hello, " + name + ", nice to meet you!"
-  }
-  
-  def say(what: String) = {
-    println(what)
+    println("Hello, " + readLine("What is your name? ") + ", nice to meet you!")
   }
 }
